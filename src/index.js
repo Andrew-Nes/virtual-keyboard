6aefac5b1cb7a4db870de774a9f180a19bc72752
Andrei Niasmachny <Cejosoll@gmail.com>
2023-04-29 11:55:25 +0300

    feat: add favicon

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
 import './style.scss';
+
+const htmlBody = document.body;
+const htmlHead = document.head;
+
+// favicon
+const favicon = document.createElement('link');
+favicon.src = '../public/favicon.ico';
+favicon.rel = 'shortcut icon';
+favicon.type = 'image/x-icon';
+htmlHead.append(favicon);
